cf9c30660625650f10fb3b9977ce23b22b983fa8
aidinkobonov <aidinkobonov@gmail.com>
2020-12-17 15:08:07 -0600

    modded sns in main.tf

--- main.tf
+++ main.tf
@@ -6,6 +6,6 @@ module "config" {
   config_logs_bucket                     = var.config_logs_bucket
   check_acm_certificate_expiration_check = var.check_acm_certificate_expiration_check
   acm_days_to_expiration                 = var.acm_days_to_expiration
-  config_sns_topic_arn                   = var.config_sns_topic_arn
+  config_sns_topic_arn                   = aws_sns_topic.config.arn
 
 }
