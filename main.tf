3b97a0744f63ad6c8309a199c05f2b33d99a61e4
aidinkobonov <aidinkobonov@gmail.com>
2020-12-20 23:09:36 -0600

    added module for s3

--- main.tf
+++ main.tf
@@ -1,23 +1,33 @@
-# data "aws_iam_policy_document" "config" {
-#   statement {
-#     effect = "Allow"
-#     principals {
-#       type        = "AWS"
-#       identifiers = [module.config.aws_config_role_arn]
-#     }
-#     actions   = ["SNS:Publish"]
-#     resources = [aws_sns_topic.config.arn]
-#   }
-# }
+module "config_logs" {
+  source  = "trussworks/logs/aws"
+  version = "~> 10"
 
-# resource "aws_sns_topic" "config" {
-#   name = var.config_name
-# }
+  s3_bucket_name     = var.config_logs_bucket
+  allow_config       = true
+  config_logs_prefix = "config"
+  force_destroy      = true
+}
 
-# resource "aws_sns_topic_policy" "config" {
-#   arn    = aws_sns_topic.config.arn
-#   policy = data.aws_iam_policy_document.config.json
-# }
+data "aws_iam_policy_document" "config" {
+  statement {
+    effect = "Allow"
+    principals {
+      type        = "AWS"
+      identifiers = [module.config.aws_config_role_arn]
+    }
+    actions   = ["SNS:Publish"]
+    resources = [aws_sns_topic.config.arn]
+  }
+}
+
+resource "aws_sns_topic" "config" {
+  name = var.config_name
+}
+
+resource "aws_sns_topic_policy" "config" {
+  arn    = aws_sns_topic.config.arn
+  policy = data.aws_iam_policy_document.config.json
+}
 
 # resource "aws_iam_role" "main" {
 #   name               = "${var.config_name}-role"
@@ -43,5 +53,5 @@ module "config" {
   config_logs_bucket                     = var.config_logs_bucket
   check_acm_certificate_expiration_check = var.check_acm_certificate_expiration_check
   acm_days_to_expiration                 = var.acm_days_to_expiration
- # config_sns_topic_arn                   = aws_sns_topic.config.arn
+  config_sns_topic_arn                   = aws_sns_topic.config.arn
 }
