d2a8ee2697fede3f70d58fbe25db913e5de5ff6c
aidinkobonov <aidinkobonov@gmail.com>
2020-12-20 22:52:06 -0600

    reverted back s3 bucket name

--- variables.tf
+++ variables.tf
@@ -21,7 +21,7 @@ variable "config_name" {
 variable "config_logs_bucket" {
   description = "The S3 bucket for AWS Config logs."
   type        = string
-  default     = "aws_config_bucket"
+  default     = "aws-config-bucket"
 }
 
 variable "config_sns_topic_arn" {
